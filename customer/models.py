00403430b4ba491c89ba675c2db6bc30f7d24314
Sajjad Yadollahzadeh <s.yadollahzadeh@gmail.com>
2021-09-10 07:52:08 +0430

    Add models

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Customers')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    address = models.TextField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    phone = models.CharField(max_length=100)
+    place = models.CharField(max_length=100, null=True, blank=True)
+    paid_amount = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'{self.first_name} -- {self.last_name}'
--- order/models.py
+++ order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from product.models import Product
+from customer.models import Customer
+
+
+class OrderItem(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.customer} - {self.product}'
